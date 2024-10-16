b63a162372018958c37dbc178de292bbaf5401c3
Kyrie112 <74169468+Kyrie112@users.noreply.github.com>
2024-10-16 15:10:56 +0800

    Update genrootfs.sh

--- genrootfs.sh
+++ genrootfs.sh
@@ -12,7 +12,7 @@ run_in_chroot()
 
 MIRROR=http://mirrors.kernel.org/debian
 ROOTDIR=rootfs
-PACKAGES="make gcc flex bison libelf-dev libssl-dev dwarves build-essential git automake autoconf"
+PACKAGES="make gcc flex bison libelf-dev libssl-dev dwarves autoconf automake libfuse-dev liblz4-dev liblzma-dev libtool pkg-config uuid-dev zlib1g-dev"
 if test -f kbuild-packages ; then
     PACKAGES="$PACKAGES $(cat kbuild-packages)"
 fi
